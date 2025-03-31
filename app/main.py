765ecc206a0f2466c5ea1343f003cdeeb04b9136
gsmafra <gsenamafra@gmail.com>
2025-03-30 23:07:17 -0300

    lint app

--- app/main.py
+++ app/main.py
@@ -1,29 +1,35 @@
-from flask import Flask, Blueprint, jsonify, render_template, request
 import sqlite3
 from functools import wraps
 
-app = Flask(__name__)
-app.template_folder = 'templates'
+from flask import Flask, Blueprint, jsonify, render_template, request
 
-main_blueprint = Blueprint('main', __name__)
+app = Flask(__name__)
+app.template_folder = "templates"
+
+main_blueprint = Blueprint("main", __name__)
+
 
 def handle_exceptions(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
             return f(*args, **kwargs)
+        # pylint: disable=W0718
         except Exception as e:
-            return jsonify({'error': str(e)}), 500
+            return jsonify({"error": str(e)}), 500
+
     return wrapper
 
-@main_blueprint.route('/')
-def index():
-    return render_template('index.html')
 
-@main_blueprint.route('/real-estate')
+@main_blueprint.route("/")
+def index():
+    return render_template("index.html")
+
+
+@main_blueprint.route("/real-estate")
 @handle_exceptions  # Wrap endpoint with error handling.
 def get_real_estate():
-    db_path = 'data/final/real_estate_data.db'
+    db_path = "data/final/real_estate_data.db"
     logradouro_search = request.args.get("search", "")
     numero_search = request.args.get("numero", "")
 
@@ -45,7 +51,10 @@ def get_real_estate():
         ORDER BY data_de_transacao DESC
         LIMIT 50
     """
-    cursor.execute(query, (logradouro_search, f"%{logradouro_search}%", numero_search, numero_search))
+    cursor.execute(
+        query,
+        (logradouro_search, f"%{logradouro_search}%", numero_search, numero_search),
+    )
     rows = cursor.fetchall()
 
     column_names = [description[0] for description in cursor.description]
@@ -54,7 +63,8 @@ def get_real_estate():
     conn.close()
     return jsonify(results)
 
+
 app.register_blueprint(main_blueprint)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
